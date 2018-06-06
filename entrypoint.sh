e4549bac24cfcec79be7ea256f8325c6582d5c93
Greger Wedel <gtw@hudya.no>
2018-06-06 15:47:15 +0200

    Wrong reference to pypi server

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 cd /src/
 
 echo "Testing registration on pypi and check healthcheck pypi repository"
-python setup.py register -r api_jwt
+python setup.py register -r python-hudya
 
 echo "Uploading to pypi repository"
-python setup.py sdist --format=zip upload -r api_jwt
+python setup.py sdist --format=zip upload -r python-hudya
