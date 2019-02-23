073464e9cd40d0fe9d08d4544291f3f59fd723f2
Greger Wedel <gtw@hudya.no>
2019-02-23 08:07:26 +0100

    Try to fix conf.py for readthedocs

--- conf.py
+++ conf.py
@@ -14,6 +14,7 @@
 #
 import os
 import sys
+from recommonmark.parser import CommonMarkParser
 sys.path.insert(0, os.path.abspath('.'))
 
 
@@ -52,11 +53,12 @@ templates_path = ['_templates']
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = {
-    '.rst': 'restructuredtext',
-    '.txt': 'markdown',
-    '.md': 'markdown',
+source_suffix = ['.rst', '.txt', '.md']
+
+source_parsers = {
+    '.md': CommonMarkParser,
 }
+
 # The master toctree document.
 master_doc = 'index'
 
